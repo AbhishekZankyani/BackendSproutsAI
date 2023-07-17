64297daed81434b867807219a68f50fff6586e8e
AbhishekZankyani <71398312+AbhishekZankyani@users.noreply.github.com>
2023-07-17 15:21:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+const mongoose= require('mongoose')
+const task = require('./Jobs/changeStatusJob')
+const url = process.env.DBURL
+app.use(cors())
+mongoose.connect(url,{})
+const con = mongoose.connection
+con.on('open',()=>{
+    console.log(`Connected to the database ${process.env.DBURL}`)
+})
+
+app.use(express.json())
+app.use('/',require('./routes/user'))
+app.use('/Roles',require('./routes/roles'))
+app.use('/Tasks',require('./routes/tasks'))
+
+
+
+task.start()
+
+const port = process.env.PORT||1234
+app.listen(port,()=>{
+    console.log(`server Connected go to server by line http://localhost:${port}`)
+})
